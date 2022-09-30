cdc7372ba62355918299412fd5affc0f071cd59b
kixmaldo <111089344+kixmaldo@users.noreply.github.com>
2022-09-30 17:47:59 -0300

    Update Usuario.java

--- src/com/company/poo/Usuario.java
+++ src/com/company/poo/Usuario.java
@@ -22,7 +22,7 @@ public class Usuario {
         this.estadoFisico = estadoFisico;
     }
     
-    public Usuario(String nombre, String rut, int Edad){
+    public Usuario(String nombre, String rut, int edad){
         this.nombre = nombre;
         this.rut = rut;
         this.edad = edad;
