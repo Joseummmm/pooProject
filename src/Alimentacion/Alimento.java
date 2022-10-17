17f07d2cdb26bac0ad2a7e8505e15c568ed63452
Joseummmm <88992460+Joseummmm@users.noreply.github.com>
2022-10-17 20:08:32 -0300

    se implementaron las funciones pedidas para el menu, falta en algunas clases

--- src/Alimentacion/Alimento.java
+++ src/Alimentacion/Alimento.java
@@ -2,6 +2,8 @@ package Alimentacion;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -32,72 +34,56 @@ public class Alimento {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setCantidadEnGramos(int cantidadEnGramos) {
         this.cantidadEnGramos = cantidadEnGramos;
     }
-
     public void setEnergia(float energia) {
         this.energia = energia;
     }
-
     public void setProteinas(float proteinas) {
         this.proteinas = proteinas;
     }
-
-
     public void setSodio(float sodio) {
         this.sodio = sodio;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public int getCantidadEnGramos() {
         return cantidadEnGramos;
     }
     public float getEnergia() {
         return energia;
     }
-
     public float getProteinas() {
         return proteinas;
     }
-
     public float getGrasas() {
         return grasas;
     }
-
     public float getAzucar() {
         return azucares;
     }
-
     public float getSodio() {
         return sodio;
     }
-
     public String getTipo() {
         return tipo;
     }
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-
     public void setGrasas(float grasas) {
         this.grasas = grasas;
     }
-
     public float getAzucares() {
         return azucares;
     }
-
     public void setAzucares(float azucares) {
         this.azucares = azucares;
     }
 
     //METODOS
-
     public static ArrayList<Alimento> leerAlimentosDisponibles() throws FileNotFoundException {
         ArrayList<Alimento> alimentos = new ArrayList<>();
         int cantidadAtributos = 8;
@@ -117,5 +103,148 @@ public class Alimento {
         }
         scannerAlimentos.close();
         return alimentos;
+    }
+
+    //agregar alimento a la lista de alimentos disponibles
+    public static void agregarAlimentoDisponible(ArrayList<Alimento> alimentosDisponibles, Alimento alimentoAgregar) throws IOException {
+        alimentosDisponibles.add(alimentoAgregar);
+        reescribirAlimentosDisponibles(alimentosDisponibles);
+    }
+
+    //eliminar alimento disponible
+    public static void eliminarAlimentoDisponible(ArrayList<Alimento> alimentos, Alimento alimentoEliminar) throws IOException {
+        alimentos.remove(alimentoEliminar);
+        reescribirAlimentosDisponibles(alimentos);
+    }
+
+    //mostrar alimentos disponibles por pantalla
+    public static void mostrarAlimentosDisponibles(ArrayList<Alimento> alimentos) {
+        for (Alimento x :
+                alimentos) {
+            System.out.println("Nombre: " + x.getNombre());
+            System.out.println("Tipo de alimento: " + x.getTipo());
+            System.out.println("Gramos por porcion: " + x.getCantidadEnGramos());
+            System.out.println("Energia total: " + x.getEnergia());
+            System.out.println("Proteina total: " + x.getProteinas());
+            System.out.println("Grasas totales: " + x.getGrasas());
+            System.out.println("Azucares totales: " + x.getAzucar());
+            System.out.println("Sodio total: " + x.getSodio());
+            System.out.println();
+        }
+    }
+
+    //rescribir alimentos disponibles
+    public static void reescribirAlimentosDisponibles(ArrayList<Alimento> alimentos) throws IOException {
+        String filePath = new File("").getAbsolutePath();
+        File archivoAlimentos = new File(filePath + "\\Recursos\\ElementosDisponibles\\AlimentosDisponibles.txt");
+        FileWriter writer = new FileWriter(archivoAlimentos);
+        for (Alimento alimento :
+                alimentos) {
+            writer.write("Nombre:" + alimento.getNombre() + "\n");
+            writer.write("Tipo:" + alimento.getTipo() + "\n");
+            writer.write("GramosPorcion:" + alimento.getCantidadEnGramos() + "\n");
+            writer.write("Energia:" + alimento.getEnergia() + "\n");
+            writer.write("Proteinas:" + alimento.getProteinas() + "\n");
+            writer.write("Grasas:" + alimento.getGrasas() + "\n");
+            writer.write("Azucares:" + alimento.getAzucar() + "\n");
+            writer.write("Sodio:" + alimento.getSodio() + "\n");
+        }
+        writer.close();
+    }
+
+    //obtener alimento desde la consola
+    public static Alimento obtenerAlimento() {
+        Scanner scanner = new Scanner(System.in);
+        String nombre,tipo;
+        int cantidadEnGramos;
+        float energia,proteinas,grasas,azucares,sodio;
+        System.out.println("Ingrese el nombre del alimento: ");
+        nombre = scanner.nextLine();
+        System.out.println("Ingrese el tipo de alimento: ");
+        tipo = scanner.nextLine();
+        System.out.println("Ingrese la energia total del alimento: ");
+        energia = scanner.nextFloat();
+        System.out.println("Ingrese la proteina total del alimento: ");
+        proteinas = scanner.nextFloat();
+        System.out.println("Ingrese las grasas totales del alimento: ");
+        grasas = scanner.nextFloat();
+        System.out.println("Ingrese el azucar total del alimento: ");
+        azucares = scanner.nextFloat();
+        System.out.println("Ingrese el sodio total del alimento: ");
+        sodio = scanner.nextFloat();
+        System.out.println("Ingrese la cantidad de gramos que constituyen una porcion del alimento: ");
+        cantidadEnGramos = scanner.nextInt();
+        return new Alimento(nombre,tipo,cantidadEnGramos,energia,proteinas,grasas,azucares,sodio);
+    }
+
+    //buscar alimento en alimentos disponibles, puede ser por nombre directamente o por nombre desde un objeto
+    public static Alimento buscarAlimento(ArrayList<Alimento> alimentos, Alimento alimentoBuscar) {
+        for (Alimento alimento :
+                alimentos) {
+            if (alimento.getNombre().equals(alimentoBuscar.getNombre())) {
+                return alimento;
+            }
+        }
+        return null;
+    }
+    public static Alimento buscarAlimento(ArrayList<Alimento> alimentos, String nombreAlimentoBuscar) {
+        for (Alimento alimento :
+                alimentos) {
+            if (nombreAlimentoBuscar.equals(alimento.getNombre())) {
+                return alimento;
+            }
+        }
+        return null;
+    }
+
+    //actualizar elemento, puede ser por nombre directamente o por nombre desde un objeto
+    public static void modificarAlimento(ArrayList<Alimento> alimentos, Alimento alimentoModificar) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        for (int index = 0; index < alimentos.size(); index++) {
+            if (alimentos.get(index).getNombre().equals(alimentoModificar.getNombre())) {
+                System.out.println("Ingrese el nuevo nombre del alimento: ");
+                alimentos.get(index).setNombre(scanner.nextLine());
+                System.out.println("Ingrese el nuevo tipo del alimento: ");
+                alimentos.get(index).setTipo(scanner.nextLine());
+                System.out.println("Ingrese la nueva cantidad de gramos que constituyen una porcion: ");
+                alimentos.get(index).setCantidadEnGramos(scanner.nextInt());
+                System.out.println("Ingrese la nueva cantidad de energia total: ");
+                alimentos.get(index).setEnergia(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de proteina total: ");
+                alimentos.get(index).setProteinas(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de grasas totales: ");
+                alimentos.get(index).setGrasas(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de azucares totales: ");
+                alimentos.get(index).setAzucares(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de sodio total: ");
+                alimentos.get(index).setSodio(scanner.nextFloat());
+            }
+        }
+        reescribirAlimentosDisponibles(alimentos);
+    }
+
+    public static void modificarAlimento(ArrayList<Alimento> alimentos, String nombreAlimentoModificar) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        for (int index = 0; index < alimentos.size(); index++) {
+            if (alimentos.get(index).getNombre().equals(nombreAlimentoModificar)) {
+                System.out.println("Ingrese el nuevo nombre del alimento: ");
+                alimentos.get(index).setNombre(scanner.nextLine());
+                System.out.println("Ingrese el nuevo tipo del alimento: ");
+                alimentos.get(index).setTipo(scanner.nextLine());
+                System.out.println("Ingrese la nueva cantidad de gramos que constituyen una porcion: ");
+                alimentos.get(index).setCantidadEnGramos(scanner.nextInt());
+                System.out.println("Ingrese la nueva cantidad de energia total: ");
+                alimentos.get(index).setEnergia(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de proteina total: ");
+                alimentos.get(index).setProteinas(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de grasas totales: ");
+                alimentos.get(index).setGrasas(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de azucares totales: ");
+                alimentos.get(index).setAzucares(scanner.nextFloat());
+                System.out.println("Ingrese la nueva cantidad de sodio total: ");
+                alimentos.get(index).setSodio(scanner.nextFloat());
+            }
+        }
+        reescribirAlimentosDisponibles(alimentos);
     }
 }
--- src/Ejercitacion/Ejercicio.java
+++ src/Ejercitacion/Ejercicio.java
@@ -1,7 +1,11 @@
 package Ejercitacion;
 
+import Alimentacion.Alimento;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -24,37 +28,30 @@ public class Ejercicio {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setRepeticiones(int repeticiones) {
         this.repeticiones = repeticiones;
     }
-
     public void setDuracionEstimada(int duracionEstimada) {
         this.duracionEstimada = duracionEstimada;
     }
-
-    public void setCaloriasQuemadasPorMinuto(int caloriasQuemadasPorMinuto) {
+    public void setCaloriasQuemadasPorMinuto(float caloriasQuemadasPorMinuto) {
         this.caloriasQuemadasPorMinuto = caloriasQuemadasPorMinuto;
     }
-
-    //getter
     public String getNombre() {
         return nombre;
     }
-
     public int getRepeticiones() {
         return repeticiones;
     }
-
     public int getDuracionEstimada() {
         return duracionEstimada;
     }
-
     public float getCaloriasQuemadasPorMinuto() {
         return caloriasQuemadasPorMinuto;
     }
 
     //METODOS
+    //leer ejercicios desde archivo de texto
     public static ArrayList<Ejercicio> leerEjerciciosDisponibles() throws FileNotFoundException {
         ArrayList<Ejercicio> ejercicios = new ArrayList<>();
         int cantidadAtributos = 4;
@@ -72,5 +69,108 @@ public class Ejercicio {
         }
         scannerEjercicios.close();
         return ejercicios;
+    }
+    //obtener ejercicio desde la consola
+    public static Ejercicio obtenerEjercicio() {
+        Scanner scanner = new Scanner(System.in);
+        String nombre;
+        int repeticiones, duracionEstimada;
+        float caloriasQuemadasPorMinuto;
+        System.out.println("Ingrese el nombre del ejercicio: ");
+        nombre = scanner.nextLine();
+        System.out.println("Ingrese el numero de repeticiones estandar del ejercicio: ");
+        repeticiones = scanner.nextInt();
+        System.out.println("Ingrese la cantidad de minutos que dura aproximadamente el ejercicio: ");
+        duracionEstimada = scanner.nextInt();
+        System.out.println("Ingrese la cantidad habitual de calorias quemadas por minuto: ");
+        caloriasQuemadasPorMinuto = scanner.nextFloat();
+        return new Ejercicio(nombre,repeticiones,duracionEstimada,caloriasQuemadasPorMinuto);
+    }
+    //agregar ejercicio a lista de ejercicios disponibles
+    public static void agregarEjercicioDisponible(ArrayList<Ejercicio> ejercicios, Ejercicio ejercicioAgregar) throws IOException {
+        ejercicios.add(ejercicioAgregar);
+        reescribirEjerciciosDisponibles(ejercicios);
+    }
+    //eliminar alimento disponible
+    public static void eliminarEjerciciosDisponible(ArrayList<Ejercicio> ejercicios, Ejercicio ejercicioEliminar) throws IOException {
+        ejercicios.remove(ejercicioEliminar);
+        reescribirEjerciciosDisponibles(ejercicios);
+    }
+    //mostrar ejercicios disponibles
+    public static void mostrarEjerciciosDisponibles(ArrayList<Ejercicio> ejercicios) {
+        for (Ejercicio x :
+                ejercicios) {
+            System.out.println("Nombre: " + x.getNombre());
+            System.out.println("Repeticiones: " + x.getRepeticiones());
+            System.out.println("Duracion estimada: " + x.getDuracionEstimada());
+            System.out.println("Calorias quemadas por minuto: " + x.getCaloriasQuemadasPorMinuto());
+            System.out.println();
+        }
+    }
+    //reescribir alimentos disponibles
+    public static void reescribirEjerciciosDisponibles(ArrayList<Ejercicio> ejercicios) throws IOException {
+        String filePath = new File("").getAbsolutePath();
+        File archivoEjercicios = new File(filePath + "\\Recursos\\ElementosDisponibles\\EjerciciosDisponibles.txt");
+        FileWriter writer = new FileWriter(archivoEjercicios);
+        for (Ejercicio ejercicio :
+                ejercicios) {
+            writer.write("Nombre:" + ejercicio.getNombre() + "\n");
+            writer.write("Repeticiones:" + ejercicio.getRepeticiones() + "\n");
+            writer.write("DuracionEstimada:" + ejercicio.getDuracionEstimada() + "\n");
+            writer.write("CaloriasQuemadasPorMinuto:" + ejercicio.getCaloriasQuemadasPorMinuto() + "\n");
+        }
+        writer.close();
+    }
+    //buscar ejercicio desde los ejercicios disponibles, puede ser por nombre directamente o por nombre desde un objeto
+    public static Ejercicio buscarEjercicio(ArrayList<Ejercicio> ejercicios, Ejercicio ejercicioBuscar) {
+        for (Ejercicio ejercicio :
+            ejercicios) {
+            if (ejercicio.getNombre().equals(ejercicioBuscar.getNombre())) {
+                return ejercicio;
+            }
+        }
+        return null;
+    }
+    public static Ejercicio buscarEjercicio(ArrayList<Ejercicio> ejercicios, String nombreEjercicioBuscar) {
+        for (Ejercicio ejercicio :
+                ejercicios) {
+            if (ejercicio.getNombre().equals(nombreEjercicioBuscar)) {
+                return ejercicio;
+            }
+        }
+        return null;
+    }
+    //modificar ejercicio, puede ser por nombre directamente o por nombre desde un objeto
+    public static void modificarEjercicio(ArrayList<Ejercicio> ejercicios, Ejercicio ejercicioModificar) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        for (int index = 0; index < ejercicios.size(); index++) {
+            if (ejercicios.get(index).getNombre().equals(ejercicioModificar.getNombre())) {
+                System.out.println("Ingrese el nuevo nombre del ejercicio: ");
+                ejercicios.get(index).setNombre(scanner.nextLine());
+                System.out.println("Ingrese el numero de repeticiones para el ejercicio: ");
+                ejercicios.get(index).setRepeticiones(scanner.nextInt());
+                System.out.println("Ingrese la nueva duracion estimada del ejercicio: ");
+                ejercicios.get(index).setDuracionEstimada(scanner.nextInt());
+                System.out.println("Ingrese la nueva cantidad de calorias quemadas por minuto: ");
+                ejercicios.get(index).setCaloriasQuemadasPorMinuto(scanner.nextFloat());
+            }
+        }
+        reescribirEjerciciosDisponibles(ejercicios);
+    }
+    public static void modificarEjercicio(ArrayList<Ejercicio> ejercicios, String nombreEjercicioModificar) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        for (int index = 0; index < ejercicios.size(); index++) {
+            if (ejercicios.get(index).getNombre().equals(nombreEjercicioModificar)) {
+                System.out.println("Ingrese el nuevo nombre del ejercicio: ");
+                ejercicios.get(index).setNombre(scanner.nextLine());
+                System.out.println("Ingrese el numero de repeticiones para el ejercicio: ");
+                ejercicios.get(index).setRepeticiones(scanner.nextInt());
+                System.out.println("Ingrese la nueva duracion estimada del ejercicio: ");
+                ejercicios.get(index).setDuracionEstimada(scanner.nextInt());
+                System.out.println("Ingrese la nueva cantidad de calorias quemadas por minuto: ");
+                ejercicios.get(index).setCaloriasQuemadasPorMinuto(scanner.nextFloat());
+            }
+        }
+        reescribirEjerciciosDisponibles(ejercicios);
     }
 }
