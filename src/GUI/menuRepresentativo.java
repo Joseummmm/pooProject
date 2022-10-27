79dc250d0113a41e41280a3619f0c5e1d58ceba6
kixmaldo <csmbcarlos@gmail.com>
2022-10-26 21:25:06 -0300

    menu finalizado

--- src/GUI/menuRepresentativo.java
+++ src/GUI/menuRepresentativo.java
@@ -1,9 +1,11 @@
 package GUI;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 import DatosUsuario.Credencial;
 import Alimentacion.Alimento;
 import Ejercitacion.Ejercicio;
+import java.util.ArrayList;
 public class menuRepresentativo {
 
     public menuRepresentativo(){
@@ -33,16 +35,53 @@ public class menuRepresentativo {
 
                 }
                 case 2 ->{
+                    ArrayList<Alimento>alimentos = null;
+                    try {
+                        alimentos = Alimento.leerAlimentosDisponibles();
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Ingrese el alimento buscado");
+                    String alimentoBuscado = dato.next();
+                    Alimento alimentoBuscar = Alimento.buscarAlimento(alimentos,alimentoBuscado);
+
+
 
                 }
                 case 3 ->{
-
+                    Alimento.obtenerAlimento();
                 }
                 case 4 ->{
+                    String nombre = dato.next();
+                    int repeticiones = dato.nextInt();
+                    int duracionEstimada = dato.nextInt();
+                    float calorias = dato.nextFloat();
+
+                    Ejercicio ejercicio = new Ejercicio(nombre,repeticiones,duracionEstimada,calorias);
+
+                    ArrayList<Ejercicio> ejercicios = null;
+                    try {
+                        ejercicios = Ejercicio.leerEjerciciosDisponibles();
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    try {
+                        Ejercicio.eliminarEjerciciosDisponible(ejercicios,ejercicio);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
 
                 }
                 case 5 ->{
-
+                    ArrayList<Alimento> alimentos = null;
+                    try {
+                        alimentos = Alimento.leerAlimentosDisponibles();
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Alimento.mostrarAlimentosDisponibles(alimentos);
                 }
                 case 0 ->{
                     System.out.println("Adios :)");
